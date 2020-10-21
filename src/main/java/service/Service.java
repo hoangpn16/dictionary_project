ffe3dec4b67f69aab40f686ba5503df4ee32dd07
PhamDung177 <kerhoangde1707@gmail.com>
2020-10-21 08:37:17 +0700

    update repository

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -5,240 +5,225 @@ import entity.Word;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import view.Controller;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Service {
-	Connection con;
+    public Connection connection;
 
-	public static Service instance;
+    public static Service instance;
 
-	public Service(Connection con) {
-		this.con = con;
-	}
+    public Service(Connection connection) {
+        this.connection = connection;
+    }
 
-	public Service() {
-	}
+    public Service() {
+    }
 
-	public static Service gI() {
-		if (Service.instance == null) {
-			Service.instance = new Service();
-		}
-		return Service.instance;
-	}
+    public static Service gI() {
+        if (Service.instance == null) {
+            Service.instance = new Service();
+        }
+        return Service.instance;
+    }
 
-	/**
-	 * Hàm kiểm tra xem từ đã có trong data chưa.
-	 *
-	 * @param word_target từ tiếng anh
-	 * @return true or false
-	 */
-	public boolean checkWordInData(String word_target) {
-		try {
-			Statement statement = con.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM `dictionary` WHERE `word`='" + word_target + "'");
-			if (resultSet.next()) {
-				return true;
-			}
-		} catch (Exception e) {
-			System.out.println("" + e);
-		}
-		return false;
-	}
+    /**
+     * Hàm kiểm tra xem từ đã có trong data chưa.
+     * chú ý pass My SQL
+     *
+     * @param word_target từ tiếng anh
+     * @return true or false
+     */
+    public boolean checkWordInData(String word_target) {
+        try {
+            Statement statement = connection.createStatement();
+            String url = "SELECT * FROM `dictionary` WHERE `word`='" + word_target + "'";
+            ResultSet resultSet = statement.executeQuery(url);
+            if (resultSet.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+        return false;
+    }
 
-	/**
-	 * Hàm tìm kiếm từ.
-	 *
-	 * @param word_target từ cần tìm kiếm
-	 * @return list các từ giống từ tìm được
-	 */
-	public List<Word> searchWord(String word_target) {
-		List<Word> listWords = new ArrayList<>();
-		try {
-			Statement statement = con.createStatement();
-			ResultSet resultSet = statement.executeQuery("SELECT * FROM `dictionary` WHERE `word`='" + word_target + "'");
-			while (resultSet.next()) {
-				Word word = new Word(resultSet.getString(2), resultSet.getString(3));
-				listWords.add(word);
-			}
+    /**
+     * Hàm kết nối đến MySQL.
+     * add dictionary_data.sql, url: @.../lib/dictionary_data.sql
+     */
+    public void loadMySQL() {
+        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found !!");
+            return;
+        }
+        System.out.println("MySQL JDBC Driver Registered!");
+        try {
+            String url = "jdbc:mysql://127.0.0.1:3306/btl_dictionary?characterEncoding=UTF-8&autoReconnect"
+                    + "=true&connectTimeout=30000&socketTimeout=30000&serverTimezone=UTC";
+            connection = DriverManager
+                    .getConnection(url, "root", "17072000");
+            System.out.println("SQL Connection to database established!");
+            instance = new Service(connection);
 
-		} catch (Exception e) {
-			System.out.println("" + e);
-		}
-		return listWords;
-	}
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            System.out.println(e);
+            return;
+        }
+    }
 
-	/**
-	 * Hàm tìm kiếm từ theo các chữ cái đầu tiên của từ.
-	 * Giớ hạn 25 từ
-	 *
-	 * @param keyword các chữ cái đầu của từ
-	 * @return list các từ tìm được
-	 */
-	public List<Word> findByCharacter(String keyword) {
-		List<Word> listWordTarget = new ArrayList<>();
-		try {
-			Statement statement = con.createStatement();
-			String sql = "SELECT * FROM `dictionary` WHERE `word` LIKE '" + keyword + "%' LIMIT 25";
-			ResultSet resultSet = statement.executeQuery(sql);
-			while (resultSet.next()) {
-				Word rs = new Word(resultSet.getString(2), resultSet.getString(3));
-				if (checkItemList(listWordTarget, rs)) {
-					listWordTarget.add(rs);
-				}
-			}
+    /**
+     * Hàm tìm kiếm từ.
+     *
+     * @param word_target từ cần tìm kiếm
+     * @return list các từ giống từ tìm được
+     */
+    public List<Word> searchWord(String word_target) {
+        List<Word> listWords = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String url = "SELECT * FROM `dictionary` WHERE `word`='" + word_target + "'";
+            ResultSet resultSet = statement.executeQuery(url);
+            while (resultSet.next()) {
+                Word word = new Word(resultSet.getString(2), resultSet.getString(3));
+                listWords.add(word);
+            }
 
-		} catch (Exception e) {
-			System.out.println("" + e);
-		}
-		return listWordTarget;
-	}
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+        return listWords;
+    }
 
-	/**
-	 * Hàm check từ đã tồn tại trong list chưa.
-	 *
-	 * @param listWords List word cần check
-	 * @param word      từ cần check
-	 * @return true or false
-	 */
-	public boolean checkItemList(List<Word> listWords, Word word) {
-		if (listWords.size() == 0) {
-			return true;
-		}
-		for (int i = 0; i < listWords.size(); i++) {
-			if (listWords.get(i).getWord_target().equals(word.getWord_target())
-					&& listWords.get(i).getWord_explain().equals(word.getWord_explain())) {
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * Hàm tìm kiếm từ theo các chữ cái đầu tiên của từ.
+     * Giớ hạn 25 từ
+     *
+     * @param keyword các chữ cái đầu của từ
+     * @return list các từ tìm được
+     */
+    public List<Word> findByCharacter(String keyword) {
+        List<Word> listWordTarget = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM `dictionary` WHERE `word` LIKE '" + keyword + "%' LIMIT 25";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                Word rs = new Word(resultSet.getString(2), resultSet.getString(3));
+                if (checkItemList(listWordTarget, rs)) {
+                    listWordTarget.add(rs);
+                }
+            }
 
-	/**
-	 * Hàm phát âm.
-	 * add thư viện freetts.jar,  url: @.../lib/freetts-1.2.1-bin/freetts-1.2.1/lib/freetts.jar
-	 *
-	 * @param text chữ cần phát âm
-	 */
-	public void speech(String text) {
-		VoiceManager voiceManager = VoiceManager.getInstance();
-		com.sun.speech.freetts.Voice syntheticVoice = voiceManager.getVoice("kevin16");
-		syntheticVoice.allocate();
-		syntheticVoice.speak(text);
-		syntheticVoice.deallocate();
-	}
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+        return listWordTarget;
+    }
 
-	/**
-	 * Hàm hiện bảng thông báo thực thi các action.
-	 *
-	 * @param info     thông báo
-	 * @param idAction id action
-	 * @param type     loại thông báo
-	 * @param word     word
-	 */
-	public void startMsgBox(String info, int idAction, int type, Word word) {
-		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-		alert.setTitle("Notification");
-		alert.setHeaderText(info);
+    /**
+     * Hàm check từ đã tồn tại trong list chưa.
+     *
+     * @param listWords List word cần check
+     * @param word      từ cần check
+     * @return true or false
+     */
+    private boolean checkItemList(List<Word> listWords, Word word) {
+        if (listWords.size() == 0) {
+            return true;
+        }
+        for (int i = 0; i < listWords.size(); i++) {
+            if (listWords.get(i).getWord_target().equals(word.getWord_target())
+                    && listWords.get(i).getWord_explain().equals(word.getWord_explain())) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-		ButtonType buttonTypeYes = new ButtonType("Yes", ButtonBar.ButtonData.YES);
-		ButtonType buttonTypeNo = new ButtonType("No", ButtonBar.ButtonData.NO);
-		ButtonType buttonTypeOK = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+    /**
+     * Hàm phát âm.
+     * add thư viện freetts.jar,  url: @.../lib/freetts-1.2.1-bin/freetts-1.2.1/lib/freetts.jar
+     *
+     * @param text chữ cần phát âm
+     */
+    public void speech(String text) {
+        VoiceManager voiceManager = VoiceManager.getInstance();
+        com.sun.speech.freetts.Voice syntheticVoice = voiceManager.getVoice("kevin16");
+        syntheticVoice.allocate();
+        syntheticVoice.speak(text);
+        syntheticVoice.deallocate();
+    }
 
-		if (type == 0) {
-			alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
-		} else {
-			alert.getButtonTypes().setAll(buttonTypeOK);
-		}
+    /**
+     * Hàm hiện thông báo.
+     *
+     * @param info string
+     */
+    public void startMsgBox(String info) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Notification");
+        alert.setHeaderText(info);
+        ButtonType buttonTypeOK = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        alert.getButtonTypes().setAll(buttonTypeOK);
+        alert.showAndWait();
+    }
 
-		Optional<ButtonType> rs = alert.showAndWait();
+    /**
+     * Hàm thêm từ vào data.
+     *
+     * @param word_target  từ tiếng anh
+     * @param word_explain nghĩa của từ
+     */
+    public void insertWord(String word_target, String word_explain) {
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "INSERT INTO `dictionary`(`word`,`detail`) VALUES ('"
+                    + word_target + "','" + word_explain + "') ";
+            statement.execute(sql);
+            this.startMsgBox("Đã thêm vào từ điển!");
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+    }
 
-		if (rs.get() == buttonTypeYes) {
-			System.out.println("Yes");
-			actionPerform(idAction, word);
-		} else if (rs.get() == buttonTypeNo) {
-			System.out.println("No");
-		} else {
-			System.out.println("OK");
-		}
-	}
+    /**
+     * Hàm xóa từ.
+     *
+     * @param word_target từ cần xóa
+     */
+    public void removeWord(String word_target) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeQuery("SET SQL_SAFE_UPDATES=0 ");
+            String query = "DELETE FROM `btl_dictionary`.`dictionary` WHERE `word` ='" + word_target + "'";
+            statement.executeUpdate(query);
+            this.startMsgBox("Đã xóa khỏi từ điển!");
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+    }
 
-	/**
-	 * Hàm action.
-	 *
-	 * @param idAction int
-	 * @param word     word
-	 */
-	private void actionPerform(int idAction, Word word) {
-		switch (idAction) {
-			case 1:
-				Controller.service.insertWord(word.getWord_target(), word.getWord_explain());
-				break;
-			case 2:
-				Controller.service.fixWord(word.getWord_target(), word.getWord_explain());
-				break;
-			case 3:
-				Controller.service.removeWord(word.getWord_target());
-				break;
-			default:
-				System.out.println("Not action!");
-				break;
-		}
-	}
-
-	/**
-	 * Hàm thêm từ vào data.
-	 *
-	 * @param word_target  từ tiếng anh
-	 * @param word_explain nghĩa của từ
-	 */
-	public void insertWord(String word_target, String word_explain) {
-		try {
-			Statement statement = con.createStatement();
-			String sql = "INSERT INTO `dictionary`(`word`,`detail`) VALUES ('" + word_target + "','" + word_explain + "') ";
-			statement.execute(sql);
-			Service.gI().startMsgBox("Đã thêm vào từ điển!", 0, 1, null);
-		} catch (Exception e) {
-			System.out.println("" + e);
-		}
-	}
-
-	/**
-	 * Hàm xóa từ.
-	 *
-	 * @param word_target từ cần xóa
-	 */
-	public void removeWord(String word_target) {
-		try {
-			Statement statement = con.createStatement();
-			statement.executeQuery("SET SQL_SAFE_UPDATES=0 ");
-			String query = "DELETE FROM `btl_dictionary`.`dictionary` WHERE `word` ='" + word_target + "'";
-			statement.executeUpdate(query);
-			Service.gI().startMsgBox("Đã xóa khỏi từ điển!", 0, 1, null);
-		} catch (Exception e) {
-			System.out.println("" + e);
-		}
-	}
-
-	/**
-	 * Hàm sửa word.
-	 *
-	 * @param word_target từ tiếng anh
-	 * @param detail      nghĩa của từ
-	 */
-	public void fixWord(String word_target, String detail) {
-		try {
-			Statement statement = con.createStatement();
-			statement.executeUpdate("UPDATE `dictionary` SET `detail` ='" + detail + "' WHERE `word`='" + word_target + "'");
-			Service.gI().startMsgBox("Đã sửa vào từ điển!", 0, 1, null);
-		} catch (Exception e) {
-			System.out.println("" + e);
-		}
-	}
+    /**
+     * Hàm sửa word.
+     *
+     * @param word_target  từ tiếng anh
+     * @param word_explain nghĩa của từ
+     */
+    public void fixWord(String word_target, String word_explain) {
+        try {
+            Statement statement = connection.createStatement();
+            String url = "UPDATE `dictionary` SET `detail` ='" + word_explain + "' WHERE `word`='" + word_target + "'";
+            statement.executeUpdate(url);
+            this.startMsgBox("Đã sửa vào từ điển!");
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+    }
 }
--- src/main/java/view/Controller.java
+++ src/main/java/view/Controller.java
@@ -2,266 +2,199 @@ package view;
 
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import service.Service;
 import entity.Word;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 
-import java.awt.*;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-	@FXML
-	private Circle circle_imgSearch;
+    @FXML
+    private Circle circle_imgSearch;
 
-	@FXML
-	private Circle circle_imgSound;
+    @FXML
+    private Circle circle_imgSound;
 
-	@FXML
-	private Circle circle_imgAdd;
+    @FXML
+    private Circle circle_imgAdd;
 
-	@FXML
-	private Circle circle_imgCorrect;
+    @FXML
+    private Circle circle_imgCorrect;
 
-	@FXML
-	private Circle circle_imgDelete;
+    @FXML
+    private Circle circle_imgDelete;
 
-	@FXML
-	public Button btn_sound;
+    @FXML
+    public Button btn_sound;
 
-	@FXML
-	public Button btn_add;
+    @FXML
+    public Button btn_add;
 
-	@FXML
-	public Button btn_Correct;
+    @FXML
+    public Button btn_Correct;
 
-	@FXML
-	public Button btn_delete;
+    @FXML
+    public Button btn_delete;
 
-	@FXML
-	public TextField txtWord;
+    @FXML
+    public TextField txtWord;
 
-	@FXML
-	public ListView listWord = new ListView<>();
+    @FXML
+    public ListView listWord;
 
-	@FXML
-	public TextArea txtExplain;
+    @FXML
+    public TextArea txtExplain;
 
-	List<Word> wordList = new ArrayList<>();
-	Connection connection = null;
-	public static Service service = null;
-	private int numSearch;
+    public static Stage primaryStage;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		loadMySQL();
-		setImgButton();
-		listWord.setMaxHeight(0);
-	}
+    List<Word> wordList = new ArrayList<>();
 
-	/**
-	 * Hàm kết nối đến MySQL.
-	 * add dictionary_data.sql, url: @.../lib/dictionary_data.sql
-	 */
-	private void loadMySQL() {
-		System.out.println("-------- MySQL JDBC Connection Demo ------------");
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			System.out.println("MySQL JDBC Driver not found !!");
-			return;
-		}
-		System.out.println("MySQL JDBC Driver Registered!");
-		try {
-			String url = "jdbc:mysql://127.0.0.1:3306/btl_dictionary?characterEncoding=UTF-8&autoReconnect"
-					+ "=true&connectTimeout=30000&socketTimeout=30000&serverTimezone=UTC";
-			connection = DriverManager
-					.getConnection(url, "root", "17072000");
-			System.out.println("SQL Connection to database established!");
-			service = new Service(connection);
+    private int numSearch;
 
-		} catch (SQLException e) {
-			System.out.println("Connection Failed! Check output console");
-			System.out.println(e);
-			return;
-		}
-	}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setImgButton();
+        listWord.setMaxHeight(0);
+    }
 
-	/**
-	 * Hàm delay search.
-	 * Sau 0.5 không nhập text sẽ search
-	 */
-	class delaySearch extends Thread {
-		@Override
-		public void run() {
-			int num = numSearch;
-			for (int i = 0; i < 500; i++) {
-				if (num != numSearch) {
-					return;
-				}
-				try {
-					Thread.sleep(1);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			if (num != numSearch) {
-				return;
-			}
-			actionSearch();
-		}
-	}
+    /**
+     * Hàm delay search.
+     * Sau 0.5 không nhập text sẽ search
+     */
+    class delaySearch extends Thread {
+        @Override
+        public void run() {
+            int num = numSearch;
+            for (int i = 0; i < 500; i++) {
+                if (num != numSearch) {
+                    return;
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (num != numSearch) {
+                return;
+            }
+            actionSearch();
+        }
+    }
 
-	/**
-	 * Hàm load ảnh cho các button.
-	 */
-	private void setImgButton() {
-		Image image = new Image("/images/img_search.jpg");
-		circle_imgSearch.setFill(new ImagePattern(image));
-		image = new Image("/images/img_loa.jpg");
-		circle_imgSound.setFill(new ImagePattern(image));
-		image = new Image("/images/img_them.jpg");
-		circle_imgAdd.setFill(new ImagePattern(image));
-		image = new Image("/images/img_sua.jpg");
-		circle_imgCorrect.setFill(new ImagePattern(image));
-		image = new Image("/images/img_xoa.jpg");
-		circle_imgDelete.setFill(new ImagePattern(image));
-	}
+    /**
+     * Hàm load ảnh cho các button.
+     */
+    private void setImgButton() {
+        circle_imgSearch.setFill(new ImagePattern(Images.img_search));
+        circle_imgSound.setFill(new ImagePattern(Images.img_loa));
+        circle_imgAdd.setFill(new ImagePattern(Images.img_add));
+        circle_imgCorrect.setFill(new ImagePattern(Images.img_sua));
+        circle_imgDelete.setFill(new ImagePattern(Images.img_xoa));
+    }
 
-	/**
-	 * Bắt sự kiện nhập phím.
-	 */
-	public void txtWord_onKeyReleased() {
+    /**
+     * Bắt sự kiện nhập phím.
+     */
+    public void txtWord_onKeyReleased() {
 //		this.numSearch++;
 //		new delaySearch().start();
-		actionSearch();
-	}
+        actionSearch();
+        btn_sound.setDisable(txtWord.getText().trim().isEmpty());
+    }
 
-	/**
-	 * Bắt sự kiện click vào item của listview.
-	 * set từ lên txtWord
-	 * set nghĩa lên txtExplain
-	 */
-	public void listWord_onMouseClick() {
-		if (listWord.getItems().size() == 0) {
-			return;
-		}
-		int index = listWord.getSelectionModel().getSelectedIndex();
-		txtExplain.setText(wordList.get(index).toString());
-		txtWord.setText(wordList.get(index).getWord_target().toString());
-	}
+    /**
+     * Bắt sự kiện click vào item của listview.
+     * set từ lên txtWord
+     * set nghĩa lên txtExplain
+     */
+    public void listWord_onMouseClick() {
+        if (listWord.getItems().size() == 0) {
+            return;
+        }
+        int index = listWord.getSelectionModel().getSelectedIndex();
+        txtExplain.setText(wordList.get(index).toString());
+        txtWord.setText(wordList.get(index).getWord_target().toString());
+    }
 
-	/**
-	 * Hàm search.
-	 */
-	public void actionSearch() {
-		if (txtWord.getText().equals("")) {
-			listWord.getItems().clear();
-			listWord.setMaxHeight(0);
-			txtExplain.setText("");
-			return;
-		}
-		wordList.clear();
-		listWord.getItems().clear();
-		wordList = service.findByCharacter(txtWord.getText());
-		double height = wordList.size() * 30 + 1;
-		if (height > 555) {
-			height = 555;
-		}
-		listWord.setMaxHeight(height);
-		for (int i = 0; i < wordList.size(); i++) {
-			Word word = wordList.get(i);
-			listWord.getItems().add(word.getWord_target());
-			if (i == 0 && word.getWord_target().equals(txtWord.getText())) {
-				txtExplain.setText(wordList.get(i).toString());
-				listWord.getSelectionModel().select(0);
-			}
-		}
-		if (listWord.getItems().size() == 0) {
-			txtExplain.setText("");
-		}
-	}
+    /**
+     * Hàm search.
+     */
+    public void actionSearch() {
+        if (txtWord.getText().equals("")) {
+            listWord.getItems().clear();
+            listWord.setMaxHeight(0);
+            txtExplain.setText("");
+            return;
+        }
+        wordList.clear();
+        listWord.getItems().clear();
+        wordList = Service.gI().findByCharacter(txtWord.getText());
+        double height = wordList.size() * 30 + 1;
+        if (height > 555) {
+            height = 555;
+        }
+        listWord.setMaxHeight(height);
+        for (int i = 0; i < wordList.size(); i++) {
+            Word word = wordList.get(i);
+            listWord.getItems().add(word.getWord_target());
+            if (i == 0 && word.getWord_target().equals(txtWord.getText())) {
+                txtExplain.setText(wordList.get(i).toString());
+                listWord.getSelectionModel().select(0);
+            }
+        }
+        if (listWord.getItems().size() == 0) {
+            txtExplain.setText("");
+        }
+    }
 
-	/**
-	 * Bắt sự kiện button phát âm.
-	 * Phát âm text trên txtWord
-	 */
-	public void btn_sound_click() {
-		Service.gI().speech(txtWord.getText());
-	}
+    /**
+     * Bắt sự kiện button phát âm.
+     * Phát âm text trên txtWord
+     */
+    public void btn_sound_click() {
+        Service.gI().speech(txtWord.getText());
+    }
 
-	public void btn_add_click() {
-		if (txtWord.getText().equals("")) {
-			Service.gI().startMsgBox("Vui lòng nhập từ!", 0, 1, null);
-			txtWord.requestFocus();
-			return;
-		}
-		if (service.checkWordInData(txtWord.getText())) {
-			Service.gI().startMsgBox("Dữ liệu đã tồn tại!", 0, 1, null);
-			txtWord.requestFocus();
-			return;
-		}
-		if (txtExplain.getText().equals("")) {
-			Service.gI().startMsgBox("Vui lòng nhập đầy đủ dữ liệu của từ!", 0, 1, null);
-			txtExplain.requestFocus();
-			return;
-		}
-		Word word = new Word(txtWord.getText(), txtExplain.getText());
-		Service.gI().startMsgBox("Bạn có chắc chắn là thêm vào từ điển không?", 1, 0, word);
-	}
+    /**
+     * Bắt sự kiện button add.
+     */
+    public void btn_add_click() {
+        if (!txtWord.getText().equals("") && txtExplain.getText().equals("")) {
+            AddItemWord.txtTargetAdd = txtWord.getText();
+        } else {
+            AddItemWord.txtTargetAdd = "";
+        }
+        new AddItemWord().actionStart();
+    }
 
-	/**
-	 * Hàm bắt sự kiện sửa từ.
-	 * Kiểm tra text nhập vào
-	 */
-	public void btn_Correct_click() {
-		if (txtWord.getText().equals("")) {
-			Service.gI().startMsgBox("Vui lòng nhập từ!", 0, 1, null);
-			txtWord.requestFocus();
-			return;
-		}
-		if (!service.checkWordInData(txtWord.getText())) {
-			Service.gI().startMsgBox("Dữ liệu chưa tồn tại!", 0, 1, null);
-			txtWord.requestFocus();
-			return;
-		}
-		if (txtExplain.getText().equals("")) {
-			Service.gI().startMsgBox("Vui lòng nhập đầy đủ dữ liệu của từ!", 0, 1, null);
-			txtExplain.requestFocus();
-			return;
-		}
-		Word word = new Word(txtWord.getText(), txtExplain.getText());
-		Service.gI().startMsgBox("Bạn có chắc chắn là sửa vào từ điển không?", 2, 0, word);
-	}
+    /**
+     * Bắt sự kiện button correct.
+     */
+    public void btn_Correct_click() {
+        FixItemWord.txtTarget = txtWord.getText();
+        new FixItemWord().actionStart();
+    }
 
-	/**
-	 * Bắt sự kiện xóa từ.
-	 * Kiểm tra text nhập vào
-	 */
-	public void btn_delete_click() {
-		if (txtWord.getText().equals("")) {
-			Service.gI().startMsgBox("Vui lòng nhập từ!", 0, 1, null);
-			txtWord.requestFocus();
-			return;
-		}
-		if (!service.checkWordInData(txtWord.getText())) {
-			Service.gI().startMsgBox("Dữ liệu chưa tồn tại!", 0, 1, null);
-			txtWord.requestFocus();
-			return;
-		}
-		Word word = new Word(txtWord.getText(), txtExplain.getText());
-		Service.gI().startMsgBox("Bạn có chắc chắn là xóa ra khỏi từ điển không?", 3, 0, word);
-	}
+    /**
+     * Bắt sự kiện button delete.
+     */
+    public void btn_delete_click() {
+        if (!txtWord.getText().equals("") && !txtExplain.getText().equals("")) {
+            DeleteItemWord.txtTargetdelete = txtWord.getText();
+        } else {
+            DeleteItemWord.txtTargetdelete = "";
+        }
+        new DeleteItemWord().actionStart();
+    }
+
 }
